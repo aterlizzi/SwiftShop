aaac157375047371f20314aa5664cb434e15d606
Aidan Terlizzi <act5@rice.edu>
2022-06-15 13:25:03 -0400

    Initial Commit

---
+++ SwiftShopDemo/SwiftShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftShopDemoApp.swift
+//  SwiftShopDemo
+//
+//  Created by Aidan Terlizzi on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
