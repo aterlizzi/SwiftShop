ca5dcb8549ddba85e88465b774c90dc86c128d19
Aidan Terlizzi <act5@rice.edu>
2022-06-15 14:27:36 -0400

    UI implementation without apple pay

---
+++ SwiftShopDemo/CartManager.swift
@@ -0,0 +1,23 @@
+//
+//  CartManager.swift
+//  SwiftShopDemo
+//
+//  Created by Aidan Terlizzi on 6/15/22.
+//
+
+import Foundation
+
+class CartManager: ObservableObject {
+    @Published private(set) var products: [Product] = []
+    @Published private(set) var total: Int = 0
+    
+    func addToCart(product: Product) {
+        products.append(product)
+        total += product.price
+    }
+    
+    func removeFromCart(product: Product) {
+        products = products.filter { $0.id != product.id }
+        total -= product.price
+    }
+}
---
+++ SwiftShopDemo/Views/CartView.swift
@@ -0,0 +1,41 @@
+//
+//  CartView.swift
+//  SwiftShopDemo
+//
+//  Created by Aidan Terlizzi on 6/15/22.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    @EnvironmentObject var cartManager: CartManager
+    
+    var body: some View {
+        ScrollView {
+            if cartManager.products.count > 0 {
+                ForEach(cartManager.products, id: \.id) { product in
+                    ProductRow(product: product)
+                }
+                
+                HStack {
+                    Text("Your cart total is")
+                    Spacer()
+                    Text("$\(cartManager.total).00")
+                        .bold()
+                }
+                .padding()
+            } else {
+                Text("Your cart is empty")
+            }
+                    }
+        .navigationTitle(Text("My Cart"))
+        .padding(.top)
+    }
+}
+
+struct CartView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartView()
+            .environmentObject(CartManager())
+    }
+}
